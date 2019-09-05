6193f757faa45c6f6b77e3f46afe7aa80289daf8
Jacky Bourgeois <j.bourgeois@tudelft.nl>
2019-09-05 11:12:24 +0200

    update logout

--- server.ts
+++ server.ts
@@ -6,17 +6,23 @@ import { ngExpressEngine } from '@nguniversal/express-engine';
 // Import module map for lazy loading
 import { provideModuleMap } from '@nguniversal/module-map-ngfactory-loader';
 
-import * as express from 'express';
-import { join } from 'path';
+
+
+// .env
 import * as dotenv from 'dotenv';
 import * as findconfig from 'find-config';
-import * as cookieParser from 'cookie-parser'
-import * as bodyParser from 'body-parser'
-import * as session from 'express-session'
-import * as refresh from 'passport-oauth2-refresh'
-import * as passport from 'passport'
-import {Strategy,RouterAPI} from '@datacentricdesign/sdk-js'
-import * as cors from 'cors'
+dotenv.config({ path: findconfig('.env') });
+
+
+import * as express from 'express';
+import { join } from 'path';
+import * as cookieParser from 'cookie-parser';
+import * as bodyParser from 'body-parser';
+import * as session from 'express-session';
+import * as refresh from 'passport-oauth2-refresh';
+import * as passport from 'passport';
+import {Strategy, RouterAPI} from '@datacentricdesign/sdk-js';
+import * as cors from 'cors';
 
 // Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
@@ -26,10 +32,10 @@ const app = express();
 
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-//Domino for window undefined
+// Domino for window undefined
 const domino = require('domino');
 const fs = require('fs');
-const template = fs.readFileSync(join(DIST_FOLDER, 'browser','subject','index.html')).toString();
+const template = fs.readFileSync(join(DIST_FOLDER, 'browser', 'subject', 'index.html')).toString();
 const win = domino.createWindow(template);
 global['window'] = win;
 global['document'] = win.document;
@@ -51,24 +57,21 @@ app.engine('html', ngExpressEngine({
 }));
 
 app.set('view engine', 'html');
-app.set('views', join(DIST_FOLDER, 'browser','subject'));
+app.set('views', join(DIST_FOLDER, 'browser', 'subject'));
 
 // Server static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser'), {
   maxAge: '1y'
 }));
 
-//.env
-dotenv.config({ path: findconfig('.env') })
-
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = process.env.BASE_URL || ""
+const baseUrl = process.env.BASE_URL || '';
 
-const redirectUrl = process.env.BASE_URL || ""
-//const redirectUrl = "/subject"
+const redirectUrl = process.env.BASE_URL || '';
+// const redirectUrl = "/subject"
 
-const google_maps_key = process.env.MAPS_KEY
+const google_maps_key = process.env.MAPS_KEY;
 
 const strategyOptions = {
       authorizationURL: process.env.OAUTH2_AUTH_URL,
@@ -80,26 +83,26 @@ const strategyOptions = {
       state: true,
       scope: ['offline', 'openid', 'profile', 'dcd:things', 'dcd:persons']
     };
-  
+
 passport.use('oauth2', new Strategy(strategyOptions,
       (accessToken, refreshToken, profile, cb) => cb(null, {accessToken, profile})
 ));
 
 passport.use('refresh', refresh);
-    
+
 passport.serializeUser((user, done) => {
-      done(null, JSON.stringify(user))
+      done(null, JSON.stringify(user));
 });
-    
+
 passport.deserializeUser((user, done) => {
-      done(null, JSON.parse(user))
+      done(null, JSON.parse(user));
 });
-    
-app.use(cors())
+
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-    
+
 // These are middlewares required by passport js
 app.use(session({
         secret: 'keyboard cat',
@@ -109,58 +112,58 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-    
-    
+
+
 // This is a middleware that checks if the user is authenticated. It also remembers the URL so it can be used to
 // redirect to it after the user authenticated.
 const checkAuthentication = (req, res, next) => {
         // The `isAuthenticated` is available because of Passport.js
         if (!req.isAuthenticated()) {
             req.session.redirectTo = req.url;
-          res.redirect(redirectUrl+'/auth');
-            return
+          res.redirect(redirectUrl + '/auth');
+            return;
         }
-        next()
+        next();
 };
-  
+
 // These routes use the Universal engine
-app.get(baseUrl+'/',checkAuthentication,
+app.get(baseUrl + '/', checkAuthentication,
   async (req, res, next) => {
-      console.log('baseUrl')
-      console.log('token',req['user'].accessToken)
+      console.log('baseUrl');
+      console.log('token', req['user'].accessToken);
       res.render('index', { req });
 });
-// page because the redirection of '/*' crash beacuase there are many other redirection  
-app.get(baseUrl+'/page/*',checkAuthentication,
+// page because the redirection of '/*' crash beacuase there are many other redirection
+app.get(baseUrl + '/page/*', checkAuthentication,
 async (req, res, next) => {
-      console.log('page')
+      console.log('page');
       res.render('index', { req });
 });
-  
-app.get(redirectUrl+'/auth', passport.authenticate('oauth2'));
-  
-app.get(redirectUrl+'/auth/callback',
-  
+
+app.get(redirectUrl + '/auth', passport.authenticate('oauth2'));
+
+app.get(redirectUrl + '/auth/callback',
+
 passport.authenticate('oauth2',
   {failureRedirect: '/auth', failWithError: true}),
   (req, res) => {
   // After success, redirect to the page we came from originally
   console.log('/auth/callback ' + req['session'].redirectTo);
-  res.redirect(req['session'].redirectTo)
+  res.redirect(req['session'].redirectTo);
   }
 );
-  
-//Recup mapsKey
-app.get(baseUrl+'/mapsKey',checkAuthentication
-  ,(req, res) => {
-    console.log('mapsKey')
+
+// Recup mapsKey
+app.get(baseUrl + '/mapsKey', checkAuthentication
+  , (req, res) => {
+    console.log('mapsKey');
     res.send(
-      {key:google_maps_key}
-      )
+      {key: google_maps_key}
+      );
 });
-  
-//API
-app.use(baseUrl+'/api', checkAuthentication, RouterAPI);
+
+// API
+app.use(baseUrl + '/api', checkAuthentication, RouterAPI);
 
 // Start up the Node server
 app.listen(PORT, () => {
