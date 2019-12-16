3629c7556ac8fe0a970ec9a9ce026fb3a741bcf9
livein21st <niravpmalsattar@gmail.com>
2019-12-16 15:00:31 +0100

    importing ui-anguar folder

---
+++ projects/ui-angular/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of ui-angular
+ */
+
+export * from './dcd/ui-angular.service';
+export * from './dcd/ui-angular.component';
+export * from './dcd/ui-angular.module';
+export * from './dcd/classes'
+export * from './dcd/http-client.service'
