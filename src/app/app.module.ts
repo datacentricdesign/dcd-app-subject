36e4e79d9feaa62a68e9e77504c981566046e955
livein21st <niravpmalsattar@gmail.com>
2020-04-17 20:13:41 +0200

    updating ui-angular-0.2.1-dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,46 +1,44 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {RouterModule} from '@angular/router';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { DatePipe } from '@angular/common'
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DatePipe } from '@angular/common';
 
 //Components
-import {AppComponent} from './app.component';
-import {HomeComponent} from './home/home.component';
-import {TasksComponent} from './tasks/tasks.component';
-import { NavbarComponent} from './navbar/navbar.component';
-import {UserComponent} from './user/user.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { UserComponent } from './user/user.component';
 
 //Http
-import {HttpClientModule} from '@angular/common/http';
-import {TransferHttpCacheModule} from '@nguniversal/common';
+import { HttpClientModule } from '@angular/common/http';
+import { TransferHttpCacheModule } from '@nguniversal/common';
 
 // MatUI
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatTableModule} from '@angular/material/table';
-import {MatDialogModule} from '@angular/material/dialog';
-import {MatInputModule} from '@angular/material';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 //prime-ng
-import {ButtonModule} from 'primeng/button';
-import {DialogModule} from 'primeng/dialog';
-import {CalendarModule} from 'primeng/calendar';
-import {MultiSelectModule} from 'primeng/multiselect';
-import {SelectButtonModule} from 'primeng/selectbutton';
+import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
+import { CalendarModule } from 'primeng/calendar';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { SelectButtonModule } from 'primeng/selectbutton';
 
 //Vertical timeline
 import { VerticalTimelineModule } from 'angular-vertical-timeline';
 
 //@datacentricdesign/ui-angular
-import {UiAngularModule} from '@datacentricdesign/ui-angular';
+import { UiAngularModule } from '@datacentricdesign/ui-angular';
 
 //To DELETE
 /*import {HttpClientService} from './http-client.service'*/
-
-
 
 @NgModule({
   declarations: [
@@ -48,16 +46,16 @@ import {UiAngularModule} from '@datacentricdesign/ui-angular';
     HomeComponent,
     TasksComponent,
     NavbarComponent,
-    UserComponent
+    UserComponent,
   ],
   imports: [
-    BrowserModule.withServerTransition({appId: 'my-app'}),
+    BrowserModule.withServerTransition({ appId: 'my-app' }),
     BrowserAnimationsModule,
     RouterModule.forRoot([
-      {path : 'page/home', component : HomeComponent, pathMatch: 'full' },
-      {path : 'page/user', component : UserComponent, pathMatch: 'full' },
-      {path : 'page/tasks', component : TasksComponent, pathMatch: 'full'},
-      {path : '**',redirectTo: '/page/home',pathMatch: 'full'},
+      { path: 'page/home', component: HomeComponent, pathMatch: 'full' },
+      { path: 'page/user', component: UserComponent, pathMatch: 'full' },
+      { path: 'page/tasks', component: TasksComponent, pathMatch: 'full' },
+      { path: '**', redirectTo: '/page/home', pathMatch: 'full' },
     ]),
     TransferHttpCacheModule,
     MatButtonModule,
@@ -75,12 +73,12 @@ import {UiAngularModule} from '@datacentricdesign/ui-angular';
     CalendarModule,
     MatFormFieldModule,
     MultiSelectModule,
-    SelectButtonModule
+    SelectButtonModule,
   ],
   providers: [
     DatePipe,
     //HttpClientService
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
